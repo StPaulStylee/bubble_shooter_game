2acdfec7b457b6e01a3d98b55e92c2f99acc36e8
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-03-15 23:15:07 -0500

    added some more broken shit

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,5 +1,19 @@
-const Bubbleshoot = window.Bubbleshoot || {};
+$(function (){
+  const game = new Bubbleshoot.Game();
+  // game.init();
+  console.log(game);
+});
+
+var Bubbleshoot = window.Bubbleshoot || {};
 Bubbleshoot.Game = (function($){
-  const Game = () => {};
+  let Game = function() {
+    this.init = () => {
+      $('.but-start-game').bind('click', startGame);
+    };
+    let startGame = function () {
+      $('.but-start-game').unbind('click');
+      Bubbleshoot.ui.hideDialog();
+    };
+  };
   return Game;
 })($);
---
+++ public/scripts/ui.js
@@ -0,0 +1,12 @@
+var Bubbleshoot = window.Bubbleshoot || {};
+Bubbleshoot.ui = (function($){
+  let ui = {
+    init: function() {
+
+    },
+    hideDialog: function(){
+      $('.dialog').fadeOut(300);
+    }
+  };
+  return ui;
+})($);
