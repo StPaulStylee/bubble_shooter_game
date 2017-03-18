6e4872d00c1c8228c8e4bf735d96af91be704b1d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-03-18 17:00:23 -0500

    start screen now functioning

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,16 +1,17 @@
 $(function (){
   const game = new Bubbleshoot.Game();
-  // game.init();
+  game.init();
   console.log(game);
 });
 
 var Bubbleshoot = window.Bubbleshoot || {};
 Bubbleshoot.Game = (function($){
   let Game = function() {
-    this.init = () => {
+    this.init = function () {
       $('.but-start-game').bind('click', startGame);
     };
     let startGame = function () {
+      console.log('clicked');
       $('.but-start-game').unbind('click');
       Bubbleshoot.ui.hideDialog();
     };
