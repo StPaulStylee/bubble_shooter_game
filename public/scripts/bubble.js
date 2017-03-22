21578fccfd577735e70ac6cf26243075d4a0fc6e
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-03-21 23:40:59 -0500

    stuff

--- public/scripts/bubble.js
+++ public/scripts/bubble.js
@@ -1,16 +1,17 @@
 var Bubbleshoot = window.Bubbleshoot || {};
 Bubbleshoot.Bubble = (function($){
   let Bubble = function(sprite) {
-    let that = this; // do I really need this line of code?
+    // let that = this; // do I really need this line of code?
     this.getSprite = function() {
-      return sprite;}
+      return sprite;
+      }
     };
-    Bubble.create = function( {
+    Bubble.create = function() {
       let sprite = $(document.createElement('div'));
-      spirte.addClass('bubble');
+      sprite.addClass('bubble');
       sprite.addClass('bubble-0');
       let bubble = new Bubble(sprite);
       return bubble;
-    });
+    };
     return Bubble;
 })($);
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -7,12 +7,20 @@ $(function (){
 var Bubbleshoot = window.Bubbleshoot || {};
 Bubbleshoot.Game = (function($){
   let Game = function() {
+    let currentBubble;
     this.init = function () {
       $('.but-start-game').bind('click', startGame);
     };
     let startGame = function () {
       $('.but-start-game').unbind('click');
       Bubbleshoot.ui.hideDialog();
+      currentBubble = getNextBubble();
+    };
+    let getNextBubble = function() {
+      let bubble = Bubbleshoot.Bubble.create();
+      bubble.getSprite().addClass('current-bubble');
+      $('#board').append(bubble.getSprite());
+      return bubble;
     };
   };
   return Game;
